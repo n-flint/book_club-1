74f8c093a58bc05d3598a7a849ab747b177d910f
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-03-20 08:19:09 -0600

    Updated tests and code for navbar feature and routes

--- spec/features/test_a_user_can_see_a_navbar_spec.rb
+++ spec/features/test_a_user_can_see_a_navbar_spec.rb
@@ -1,22 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe "A user visits a page" do
-    describe "each page" do
+RSpec.describe "As a visitor" do
+    describe 'when I visit the home page' do
         it 'has a navbar' do
-
-            visit '/books'
-
-            within '.topnav'
-
-            expect(page).to have_content("Home")
-            expect(page).to have_content("Books")
-
             visit '/'
+            within '.topnav'
+            expect(page).to have_link("Home")
+            expect(page).to have_link("Books")
+        end
 
+        it 'can redirect to home page by clicking link' do 
+            visit '/'
+            click_link('Home')
+            page.status_code.should be 200
+            expect(current_path).to eq('/')
+        end
+
+        it 'can redirect to the books page by clicking link' do
+            visit '/'
             within '.topnav'
 
-            expect(page).to have_content("Home")
-            expect(page).to have_content("Books")
+            click_link('Books')
+            page.status_code.should be 200
+            expect(current_path).to eq("/books")
+        end     
+    end
+
+    describe 'when I visit the books page' do
+        it 'has a navbar' do
+            visit '/books'
+            within '.topnav'
+            expect(page).to have_link("Home")
+            expect(page).to have_link("Books")
+        end
+        
+        it 'can redirect to books page by clicking link' do 
+            visit '/books'
+            click_link('Books')
+            page.status_code.should be 200
+            expect(current_path).to eq('/books')
+        end
+
+        it 'can redirect to home page by clicking link' do 
+            visit '/books'
+            click_link('Home')
+            page.status_code.should be 200
+            expect(current_path).to eq('/')
         end
     end
 end
