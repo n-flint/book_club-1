f605cf1f66146ee0576946e2748742b4d844a86d
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-03-20 11:58:19 -0600

    Updated testing with route helpers

--- spec/features/test_a_user_can_see_a_navbar_spec.rb
+++ spec/features/test_a_user_can_see_a_navbar_spec.rb
@@ -3,49 +3,49 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
     describe 'when I visit the home page' do
         it 'has a navbar' do
-            visit '/'
+            visit root_path
             within '.topnav'
             expect(page).to have_link("Home")
             expect(page).to have_link("Books")
         end
 
         it 'can redirect to home page by clicking link' do 
-            visit '/'
+            visit root_path
             click_link('Home')
             page.status_code.should be 200
-            expect(current_path).to eq('/')
+            expect(current_path).to eq(root_path)
         end
 
         it 'can redirect to the books page by clicking link' do
-            visit '/'
+            visit root_path
             within '.topnav'
 
             click_link('Books')
             page.status_code.should be 200
-            expect(current_path).to eq("/books")
+            expect(current_path).to eq(books_path)
         end     
     end
 
     describe 'when I visit the books page' do
         it 'has a navbar' do
-            visit '/books'
+            visit books_path
             within '.topnav'
             expect(page).to have_link("Home")
             expect(page).to have_link("Books")
         end
         
         it 'can redirect to books page by clicking link' do 
-            visit '/books'
+            visit books_path
             click_link('Books')
             page.status_code.should be 200
-            expect(current_path).to eq('/books')
+            expect(current_path).to eq(books_path)
         end
 
         it 'can redirect to home page by clicking link' do 
-            visit '/books'
+            visit books_path
             click_link('Home')
             page.status_code.should be 200
-            expect(current_path).to eq('/')
+            expect(current_path).to eq(root_path)
         end
     end
 end
