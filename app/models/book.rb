8c2727423405cd838f09f5aba7d8fabc9d83c3bd
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-03-22 22:25:28 -0600

    adds highest_three_reviews instance method and test

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
     has_many :reviews
     has_many :bookauthors
     has_many :authors, through: :bookauthors
+
+    def highest_three_reviews
+      reviews.order(:score).last(3).reverse
+    end
 end
