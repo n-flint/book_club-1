e576f2c857c2ca8004f40e1bfd3a3d2d95787f7a
Noah Flint <noah.flint24@gmail.com>
2019-03-24 20:41:13 -0600

    refactored to titleize username in model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
     belongs_to :book
     validates_presence_of :heading, :score, :full_review, :username, :book_id
 
+    before_save {self.username = self.username.titleize}
+
   def self.newest
     order(created_at: :asc)
   end
