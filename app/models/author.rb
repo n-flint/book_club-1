d1075e931800a93ff0266f81b747b8ee3bc1774e
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-03-24 23:06:35 -0600

    completes #41

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
     validates_presence_of :name
-    has_many :bookauthors
+    has_many :bookauthors, dependent: :delete
     has_many :books, through: :bookauthors
 
     before_save { self.name = self.name.titleize }
