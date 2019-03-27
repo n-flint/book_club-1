675eef5027a063a65252b12edfeaf7a4bf3cb8eb
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-03-26 18:41:06 -0600

    Updates author show page to render without reviews
    Co-authored-by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,9 @@
 class AuthorsController < ApplicationController
     def show
         @author = Author.find(params[:id])
+        @num_reviews = @author.books.map do |book|
+          book.reviews.count
+        end
     end
 
     def destroy
